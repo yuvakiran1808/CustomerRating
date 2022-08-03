ba6cae4c2518ab96269f41aba79fe8dfc89a91f9
yuvakiran1808 <pyuvasaikiran@gmail.com>
2022-08-03 06:06:04 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const radioButtons =  document.querySelectorAll('input[name="rate"]');
+const hiddenSection  = document.querySelector('.form-section');
+const nextBtn = document.querySelector('.next-btn');
+console.log(radioButtons);
+console.log(hiddenSection);
+var a= 0;
+function checkradio(){
+    for(var i=0;i<radioButtons.length;i++)
+    {
+
+        if(radioButtons[i].checked)
+        {
+            hiddenSection.classList.remove("display");
+            nextBtn.classList.add("display");
+            a=1;
+            break;
+        }
+    }
+    if(a==0)
+    {
+        hiddenSection.classList.add("display");
+        nextBtn.classList.remove("display");
+    }
+}
+
